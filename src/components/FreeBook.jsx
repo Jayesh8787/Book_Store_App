8f7bff52008d24257b6c973ffc4bdb18650d9676
JAYESH  KARDILE <jayeshkardile8787@gmail.com>
2024-10-09 14:53:12 +0530

    add project and fix issue

---
+++ src/components/FreeBook.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Cards from "./Cards";
+
+function FreeBook() {
+  const [book, setBook] = useState([]);
+  const getBook = async () => {
+    try {
+      const responce = await axios.get("http://localhost:4001/book");
+      console.log(responce.data);
+      const data = responce.data.filter((data) => data.category === "Free");
+      console.log(data);
+      setBook(data);
+    } catch (error) {
+      console.log("Error: " + error);
+    }
+  };
+  useEffect(() => {
+    getBook();
+  }, [setBook]);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const isEmpty = book.length === 0;
+
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-bold text-red-600 text-2xl pb-2">Free Offered Courses</h1>
+          <p>
+          Here are some of our handpicked titles offered at a special price for a limited period. Hurry the deals run out soon...
+          </p>
+        </div>
+
+        {!isEmpty && (
+          <div>
+            <Slider {...settings}>
+              {book.map((item) => (
+                <Cards key={item.id} item={item} />
+              ))}
+            </Slider>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default FreeBook;
